612832c6eaf3538110e04a251d02788558f9d97e
kushalru <kushalru@gmail.com>
2016-09-26 12:35:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,8 +6,8 @@ element.innerHTML ='mister';
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight () {
-    marginLeft = marginLeft = 5;
-    img.style.marginLeft = marginLeft ='px';
+    marginLeft = marginLeft + 5;
+    img.style.marginLeft = marginLeft +'px';
 }
 img.onclick = function() {
 var interval = setInterval(moveRight, 50);   
