6e4060fbbc5a326fa7615b814f125f2378503f52
kushalru <kushalru@gmail.com>
2016-10-03 19:08:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,8 +24,6 @@ button.onclick = function () {
     request.send(null);
 };
 // Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
      // Create a request object
@@ -51,6 +49,8 @@ submit.onclick = function () {
         };
     
     // Make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
    request.open('GET', 'http://kushalru.imad.hasura-app.io/submit-name?name='+ name, true);
     request.send(null);
 
