471fe70850d3e8e5503765b84ed2f0e462ebe9dc
kushalru <kushalru@gmail.com>
2016-11-02 19:04:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user:'kushalru',
+    database:'kushalru',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 var articles = {
@@ -69,6 +78,19 @@ return htmlTemplate;
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db',function (req,res){
+ //make a select request
+ // return a response with the results
+ pool.query('SELECT * FROM test',function(err, reults){
+     if (err) {
+         res.status(500).send(err.toString());
+     } else {
+         res.send(JSON.stringify(result));
+     }
+ });
 });
 var counter = 0;
 app.get('/counter', function(req, res){
